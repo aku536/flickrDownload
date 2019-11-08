03dd9b1bfb4f730c2f2668accb8ff6ade9050d29
Kirill Afonin <aku536@mail.ru>
2019-11-08 12:15:42 +0300

    Добавлены sharedSchemes проекта

---
+++ flickrDownload/flickrDownloadFramework/ImageModel.swift
@@ -0,0 +1,19 @@
+//
+//  ImageModel.swift
+//  flickrDownload
+//
+//  Created by Кирилл Афонин on 08/11/2019.
+//  Copyright © 2019 Кирилл Афонин. All rights reserved.
+//
+
+import UIKit
+
+struct ImageModel {
+    let path: String
+    let description: String
+}
+
+struct ImageViewModel {
+    let description: String
+    let image: UIImage
+}
